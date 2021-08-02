f03ef16a8a4e0f7f6993793cb71d76d28b5b77af
gitsheerin <87943288+gitsheerin@users.noreply.github.com>
2021-08-02 11:27:21 -0400

    Delete code.java
    wrong code

--- another/code.java
+++
@@ -1,8 +0,0 @@
-public class code {
-    public static void main(String[] args) {
-        String name = "Aysha";
-        System.out.println("Good Morning, " + name);
-        System.out.println("You are the dumbest person I know");
-    }
-    
-}
